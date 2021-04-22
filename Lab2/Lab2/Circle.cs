40ad37a05681872a8970dd56dbe9ea549b299fd7
KaterinaSazonova <82824279+KaterinaSazonova@users.noreply.github.com>
2021-04-22 22:17:08 +0300

    Add Lab2
    HW to OOP

---
+++ Lab2/Lab2/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Circle : Figure
+{
+	private int radius;
+	public int Radius { get { return radius; } set { this.radius = value; } }
+    public override void Area()
+    {
+        double pi = 3.14;
+        double a = radius * radius * pi;
+        Console.WriteLine($"Площадь круга равна {a}");
+    }
+
+    public override void Perimeter()
+    {
+        double pi = 3.14;
+        double a = 2 * radius * pi;
+        Console.WriteLine($"Длина окружности равна {a}");
+    }
+    public override void Draw()
+    {
+        Console.WriteLine($"Вы нарисовали круг с радиусом =  {radius}");
+    }
+    public Circle (int r)
+    {
+        radius = r;       
+    }
+}
---
+++ Lab2/Lab2/Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+ class Square : Figure
+{
+    private int side;
+    public int Side { get { return side;  } set { this.side = value;  } }
+	public override void Area()
+    {
+        int a = side * side;
+        Console.WriteLine($"Площадь квадрата равна {a}");
+    }
+    public override void Perimeter()
+    {
+        int a = side * 4;
+        Console.WriteLine($"Периметр квадрата равен {a}");
+    }
+    public override void Draw()
+    {
+        Console.WriteLine($"Вы нарисовали квадрат со стороной =  {side}");
+    }
+}
+
