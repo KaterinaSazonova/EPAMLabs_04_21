3cf8f4607a91570857d23cc269b1f720ada1f396
KaterinaSazonova <i.kati1616@gmail.com>
2021-04-29 21:59:59 +0300

    Add Exception

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -9,21 +9,37 @@ namespace Lab2
         static void Main(string[] args)
         {
 
+           
             List<Square> s = new List<Square>
             {
                 new Square() {Side=10, СolorFilling="green", СolorLine="yellow"},
-                new Square() {Side=10, СolorFilling="blue", СolorLine="yellow"},
-                new Square() {Side=20, СolorFilling="green", СolorLine="yellow"},
+                new Square() {Side=10, СolorFilling="blue", СolorLine="yellow"},                
                 new Square() {Side=20, СolorFilling="blue", СolorLine="blue"},
-               // new Square() {Side=0, СolorFilling="blue", СolorLine="blue"}
+                new Square() {Side=0, СolorFilling="green", СolorLine="blue"}
             };
 
+            try
+            {
+                foreach (var temp in s)
+                {if (temp.Side == 0)
+                    {
+                        throw new Exception("Квадрата со длиной стороны = 0 не существует. Установлено значение по умолчанию: длина стороны квадрата = 1");
+                        temp.Side = 1;
+                    }                    
+                }
+                    
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e.Message}");
+            }
+            Console.Read();
 
             foreach (var temp in s)
             {
                 Console.WriteLine($"Квадрат со стороной {temp.Side}, нарисованный {temp.СolorLine} карандашом, закрашенный {temp.СolorFilling} цветом");
-                //temp.Perimeter();
-                //temp.Area();                
+                temp.Perimeter();
+                temp.Area();
             };
 
             List<Square> s1 = s.Where(t => t.Side == 10).ToList();
@@ -32,6 +48,8 @@ namespace Lab2
             {
                 Console.WriteLine($"Квадрат со стороной {temp.Side}, нарисованный {temp.СolorLine} карандашом, закрашенный {temp.СolorFilling} цветом");
             }
+
+
 
         }
     }
