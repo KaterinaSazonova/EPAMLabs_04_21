85952deee190e73fa54d471e44879a33867b9bfb
KaterinaSazonova <i.kati1616@gmail.com>
2021-05-06 14:46:56 +0300

    HW 5 Tests

---
+++ TestProject/TestProject/TestsSquare.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace TestProject
+{
+    public class TestsSquare
+    {
+        [Test, Combinatorial]
+        public void TestAreaSquare ( [Range(0, 20)] int x )
+
+        {
+            var temp = GeometricFigures.Square.GetArea(x);
+            Assert.AreEqual(ExpectedResult.GetAreaSquare(x), temp);
+        }
+
+        [Test, Combinatorial]
+        public void TestLengthRectangle([Range(0, 20)] int x)
+        {
+            var temp = GeometricFigures.Square.GetLength(x);
+            Assert.AreEqual(ExpectedResult.GetLengthSquare(x), temp);
+        }
+    }
+}
---
+++ TestProject/TestProject/TestsTriangle.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace TestProject
+{
+    public class TestsTriangle
+    {
+
+        [Test, Combinatorial]
+        public void TestAreaTriangle (
+            [Range(0, 20)] int x,
+            [Range(0, 20)] int y,
+            [Range(0, 20)] int z)
+
+        {
+            var temp = GeometricFigures.Triangle.GetArea(x, y, z);
+            Assert.AreEqual(ExpectedResult.GetAreaTriangle(x, y, z ), temp);
+        }
+
+        [Test, Combinatorial]
+        public void TestLengthTriangle(
+            [Range(0, 20)] int x,
+            [Range(0, 20)] int y,
+            [Range(0, 20)] int z)
+
+        {
+            var temp = GeometricFigures.Triangle.GetLength(x, y, z);
+            Assert.AreEqual(ExpectedResult.GetLengthTriangle(x, y, z), temp);
+        }
+    }
+}
